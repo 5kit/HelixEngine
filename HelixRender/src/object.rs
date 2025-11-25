ce508ee872ccfed5da1f8f6f109dba852317b02b
5kit <ec24523@qmul.ac.uk>
2025-11-25 16:00:40 +0000

    Mesh loading from obj

--- HelixRender/src/object.rs
+++ HelixRender/src/object.rs
@@ -3,6 +3,75 @@ use glam::Vec3;
 use glam::Mat4;
 use glam::Quat;
 
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+#[pyclass]
+pub struct Mesh {
+    vertices: Vec<Vec3>,
+    indices: Vec<usize>,
+    polygons: Vec<Vec<usize>>,
+    face_normals: Vec<Vec3>
+}
+
+#[pymethods]
+impl Mesh {
+    #[new]
+    pub fn new() -> Self {
+        Mesh {
+            vertices: Vec::new(),
+            indices: Vec::new(),
+            polygons: Vec::new(),
+            face_normals: Vec::new()
+        }
+    }
+
+    pub fn load_obj(&mut self, path: &str)-> io::Result<()>  {
+        self.vertices.clear();
+        self.indices.clear();
+        self.polygons.clear();
+        self.face_normals.clear();
+
+        let file = File::open(path)?;
+        let reader = io::BufReader::new(file);
+
+        for line in reader.lines() {
+            let line = line?;
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            match parts[0] {
+                "v" => {
+                    // Vertex
+                    let x: f32 = parts[1].parse().unwrap(); 
+                    let y: f32 = parts[2].parse().unwrap();
+                    let z: f32 = parts[3].parse().unwrap();
+                    self.vertices.push(Vec3::new(x,y,z));
+                },
+                "f" => {
+                    // Face
+                    let mut face = Vec::new();
+                    for vertex in &parts[1..] {
+                        let v: usize = vertex.parse().unwrap() - 1; // obj uses starts index at 1
+                        face.push(v);
+                    }
+                    self.polygons.push(face);
+
+                    // Compute normals
+                    let edge1 = self.vertices[face[1]] - self.vertices[face[0]];
+                    let edge2 = self.vertices[face[2]] - self.vertices[face[0]];
+                    let normal = edge1.cross(edge2).normalize();
+                    self.face_normals.push(normal);
+
+                },
+                _ => {}
+            }
+        }
+
+        Ok(())
+    }
+}
+
+
 #[pyclass]
 pub struct Transform {
     position: Vec3,
